2baab804d713b8e176079d91bb324f7974a9ef99
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-06-17 15:20:05 +0530

    Conditional Bean

--- src/main/java/lk/ijse/dep8/spring/bean/TestBean.java
+++ src/main/java/lk/ijse/dep8/spring/bean/TestBean.java
@@ -1,7 +1,10 @@
 package lk.ijse.dep8.spring.bean;
 
+import lk.ijse.dep8.spring.MyCondition;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Component;
 
+@Conditional(MyCondition.class)
 @Component
 public class TestBean {
 
