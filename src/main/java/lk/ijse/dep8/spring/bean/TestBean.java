4cfc0a01a3f0411cc7e4a6698f3d09f5a72e1fda
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-06-17 16:23:18 +0530

    Value Injections 3

--- src/main/java/lk/ijse/dep8/spring/bean/TestBean.java
+++ src/main/java/lk/ijse/dep8/spring/bean/TestBean.java
@@ -2,44 +2,31 @@ package lk.ijse.dep8.spring.bean;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.lang.Nullable;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.Arrays;
 
-
-//@Component
+@Component
 public class TestBean {
 
-    @Value("ijse")
-    private String something;
-
-    @Value("true")
-    private boolean flag;
-
-    @Value("1500")
-    private int age;
-
     @Autowired
-    public void doSomething(@Value("1,2,3,4,5") int[] numbers){
-        System.out.println(Arrays.toString(numbers));
-    }
+    private Environment env;
 
-    @Autowired
-    public void doSomething(@Value("dula, sasitha, chamma") String[] names){
-        System.out.println(Arrays.toString(names));
-    }
-
-    public TestBean(@Value("3.14") double PI) {
-        System.out.println("TestBean() ");
-        System.out.println(PI);
+    public TestBean(@Value("${app.log.level}") String logLevel,
+                    @Value("${app.user.name}") String userName,
+                    @Value("${PATH}") String path) {
+        System.out.println(logLevel);
+        System.out.println(userName);
+        System.out.println("--------------");
     }
 
     @PostConstruct
-    public void init(){
-        System.out.println(something);
-        System.out.println(flag);
-        System.out.println(age);
+    private void init(){
+        System.out.println(env.getProperty("app.log.level"));//null
+        System.out.println(env.getRequiredProperty("app.log.level"));
+        System.out.println(env.getProperty("app.user.name"));
+        System.out.println(env.getProperty("PATH"));
+        System.out.println(env.getProperty("java.io.tmpdir"));
     }
 }
